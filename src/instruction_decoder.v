e9c051b747b28d88b36c4c8dbdd45f7ba3d21b41
FuturePhile <135601063+FuturePhile@users.noreply.github.com>
2024-10-25 12:42:43 +0200

    Add pc, program_data and ID

---
+++ src/instruction_decoder.v
@@ -0,0 +1,13 @@
+module instruction_decoder (
+  input wire [5:0] DATA,
+  output wire ACC_CE,
+  output wire [2:0] ALU_OP,
+  output wire [1:0] RF_A,
+  output wire [2:0] RF_SEL
+);
+  
+always @(DATA) begin
+  
+end
+  
+endmodule
